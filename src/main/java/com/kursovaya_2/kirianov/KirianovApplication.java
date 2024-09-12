8bafe89187b26e721db7fcfd706a90b51ca9a4b3
Nazar <epoxasmute@mail.ru>
2024-09-12 10:45:19 +0300

    Git init

---
+++ src/main/java/com/kursovaya_2/kirianov/KirianovApplication.java
@@ -0,0 +1,13 @@
+package com.kursovaya_2.kirianov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirianovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirianovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kursovaya_2/kirianov/KirianovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursovaya_2.kirianov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirianovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
